0c34b847d9c32659570a6e672982f13628183ca7
Sebastian Garcia Guerrero <seba.garcia.g@gmail.com>
2024-08-28 15:36:07 -0400

    frontend editing

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,9 +1,11 @@
-
+"use client";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-row">
+      <div className="row-span-3 xl:row-span-6 bg-gray-500"></div>
+    </div>
+  );
+};
 
 export default Dashboard;
